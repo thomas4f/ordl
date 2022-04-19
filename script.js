ff3ad59c7cfc307bdec1ba288b334b667d7eaa37
thomasedoff <51061686+thomasedoff@users.noreply.github.com>
2022-04-19 13:27:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // stuff
 var num_cols = 4;
-var num_rows = 7;
+var num_rows = 2;
 var enter_auto = false;
 var word = words_easy[Math.floor(Math.random() * words_easy.length)].toUpperCase();
 
@@ -8,7 +8,7 @@ function create_matrix() {
 	let matrix = document.createDocumentFragment();
 	let cell = 0;
 
-	for (let i = 0; i < num_rows; i++) {
+	for (let i = 1; i <= num_rows; i++) {
 		let row = document.createElement("div");
 		row.setAttribute("id", "row-" + i);
 		for (let i = 0; i < num_cols; i++) {
@@ -41,6 +41,24 @@ function create_keyboard() {
 
 		document.getElementById("keyboard").append(el_row);
 	}
+}
+
+function create_stats() {
+	let stats = document.createElement("div");
+
+	for (let i = 1; i <= num_rows; i++) {
+		let el = document.createElement("div");
+		let el2 = document.createElement("div");
+		el.innerText = i;
+		el2.setAttribute("id", "graph-" + i);
+		el2.style.width = game_stats.scores[i] / game_stats.total * 100 + "%";
+		el2.innerText = game_stats.scores[i] || 0;
+		el.append(el2);
+		stats.append(el);
+	}
+
+	document.getElementById("stats-graph").append(stats);
+
 }
 
 function handle_input(event) {
@@ -94,15 +112,10 @@ function check_guess() {
 	setTimeout(function() {
 		if (guess == word) {
 			game_state = 1;
-			document.getElementById("answer-victory").innerText = word;
-			document.getElementById("victory").classList.remove("hidden");
-			console.log("calling vic");
-			victory();
-
+			game_end();
 		} else if (current_cell >= num_cols * num_rows) {
 			game_state = 2;
-			document.getElementById("answer-loss").innerText = word;
-			document.getElementById("loss").classList.remove("hidden");
+			game_end();
 		}
 
 		guess = "";
@@ -120,6 +133,7 @@ function flip_cell(result, key, i) {
 
 function register_events() {
 	document.addEventListener('keydown', handle_input);
+	document.getElementById("new_game").addEventListener("click", function() { location.reload() });
 
 	for (let key of document.querySelectorAll("#keyboard>div>button")) {
 		key.addEventListener("click", function() {
@@ -130,16 +144,48 @@ function register_events() {
 	}
 }
 
-function victory() {
+function game_end() {
+	document.getElementById("modal").style.display = "block";
+	game_stats.total++;
+
+	if (game_state == 1) {
+		game_stats.scores[current_row] = game_stats.scores[current_row] + 1 || 1;
+		game_stats.streak = game_stats.streak + 1 || 1;
+		if (game_stats.streak > game_stats.longest_streak) game_stats.longest_streak = game_stats.streak;
+		document.getElementById("graph-" + current_row).classList.add("correct");
+		document.getElementById("stats-game_result").innerText = "Hurra!";
+		draw_confetti();
+	} else {
+		game_stats.streak = 0;
+		document.getElementById("stats-game_result").innerText = "Tyvärr ...";
+	}
+
+	setTimeout(function() {
+		for (let i = 1; i <= num_rows; i++) {
+			document.getElementById("graph-" + i).style.width = game_stats.scores[i] / game_stats.total * 100 + "%";
+			document.getElementById("graph-" + i).innerText = game_stats.scores[i] || 0;
+		}
+	}, 250);
+
+
+	document.getElementById("stat-rounds").innerText = game_stats.total;
+	document.getElementById("stat-wins").innerText = Object.values(game_stats.scores).reduce((a, b) => a + b, 0);
+	document.getElementById("stat-streak").innerText = game_stats.streak;
+	document.getElementById("stat-longest_streak").innerText = game_stats.longest_streak;
+
+	localStorage.setItem('game_stats', JSON.stringify(game_stats));
+}
+
+function draw_confetti() {
 	var canvas = document.getElementById("canvas");
 	canvas.confetti = canvas.confetti || confetti.create(canvas, {
 		resize: false
 	});
 
-	for (let i = 0; i < num_rows - current_row; i++) {
+	for (let i = 0; i <= num_rows - current_row; i++) {
 		setTimeout(function() {
 			canvas.confetti({
-				particleCount: (i + 1) ** 2,
+				particleCount: (i + 2) ** 2,
 				angle: Math.random() * (105 - 75) + 75,
 				spread: 50,
 				startVelocity: 25,
@@ -153,19 +199,25 @@ function victory() {
 		}, i * 500);
 	}
 }
-
 function main() {
 	create_matrix();
 	create_keyboard();
+	create_stats();
 	register_events();
 }
 
 var game_state = 0;
-var current_row = 0;
+var current_row = 1;
 var current_cell = 0;
 var guess = "";
 var enter_required = false;
 var del_required = false;
 var can_interact = true;
+var game_stats = JSON.parse(localStorage.getItem('game_stats')) || {
+	"scores": {},
+	"total": 0,
+	"streak": 0,
+	"longest_streak": 0
+};
 
 document.addEventListener("DOMContentLoaded", main);
